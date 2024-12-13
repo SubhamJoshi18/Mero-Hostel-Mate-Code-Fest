f9b6211b0887fcf1293d7e943cc362dfeea5627e
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2024-12-13 16:03:49 +0545

    Popular hostel page added

--- frontend-microservice/src/components/Button/PrimaryButton.tsx
+++ frontend-microservice/src/components/Button/PrimaryButton.tsx
@@ -1,8 +1,8 @@
 function PrimaryButton({ title, ...restProps }) {
   return (
     <button
-      {...restProps}
-      className="text-white text-md bg-[--btn-primary] px-6 py-2 rounded-lg hover:bg-[--btn-secondary] transition-all active:translate-y-0.5"
+    className="text-white text-md bg-[--btn-primary] px-6 py-2 rounded-lg hover:bg-[--btn-secondary] transition-all active:translate-y-0.5"
+    {...restProps}
     >
       {title}
     </button>
