d7e8b3ccaa483bec2f552240b675e38e957dd09a
deepgeneral33@gmail.com <deepgeneral33@gmail.com>
2024-12-13 15:25:21 +0545

    removed some features

--- frontend-microservice/src/pages/HomePage.tsx
+++ frontend-microservice/src/pages/HomePage.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { HeroSection } from "../components/Hero Section/HeroSection";
-import OurFeatures from "../components/Features/OurFeatures";
-import PopularHostel from "./PopularHostel";
+// import PopularHostel from "./PopularHostel";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
-      <PopularHostel />
-      <OurFeatures />
     </>
   );
 }
