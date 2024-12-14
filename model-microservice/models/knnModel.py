85e49e6ff1eaa8ee7466859e6d29355b8c8db19c
shubhamUBA <shubham.joshi@monotype.com>
2024-12-15 00:54:42 +0545

    phase4 completed

---
+++ model-microservice/models/knnModel.py
@@ -0,0 +1,11 @@
+
+
+
+
+def knn_recommend_model():
+    try:
+
+
+
+    except Exception as error:
+        print(f'Error selecting the reference models: {error}')
---
+++ model-microservice/models/ocrModel.py
@@ -0,0 +1,9 @@
+
+
+def document_ocr_model():
+    try:
+        pass
+
+
+    except Exception as error:
+        print(f'Error selecting the reference models: {error}')
